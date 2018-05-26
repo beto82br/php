ba11de5629bb7a1897dd860cce811cd087f03446
beto82br <betorpb@gmail.com>
2018-05-26 12:57:58 -0300

    resolvi o conflito

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "Olá Mundo 456!";
 
-echo "Olá Udemy!";
-=======
-echo "Olá Mundo 123!";
+echo "Olá Udemy Hcode!";
 
-echo "Olá Hcode!";
->>>>>>> 4920d6188f2498d6267da40ef0b75fde8804d3e9
 
 ?>
